f1c4459cc22fad4c512f5dc73be4804b040554b5
HTML programmer <99501534+0E2E2D0E2E2A@users.noreply.github.com>
2023-03-25 01:31:52 +0300

    не синхронизированы потоки

---
+++ lab_OOP-2-3/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace lab_OOP_2_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox_ChangeSort = new System.Windows.Forms.TextBox();
+            this.textBox_ChoiceSort = new System.Windows.Forms.TextBox();
+            this.textBox_InsertSort = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Pause = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox_Random = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_Generation = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // textBox_ChangeSort
+            // 
+            this.textBox_ChangeSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_ChangeSort.Location = new System.Drawing.Point(16, 114);
+            this.textBox_ChangeSort.Multiline = true;
+            this.textBox_ChangeSort.Name = "textBox_ChangeSort";
+            this.textBox_ChangeSort.Size = new System.Drawing.Size(365, 93);
+            this.textBox_ChangeSort.TabIndex = 0;
+            // 
+            // textBox_ChoiceSort
+            // 
+            this.textBox_ChoiceSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_ChoiceSort.Location = new System.Drawing.Point(387, 114);
+            this.textBox_ChoiceSort.Multiline = true;
+            this.textBox_ChoiceSort.Name = "textBox_ChoiceSort";
+            this.textBox_ChoiceSort.Size = new System.Drawing.Size(365, 93);
+            this.textBox_ChoiceSort.TabIndex = 1;
+            // 
+            // textBox_InsertSort
+            // 
+            this.textBox_InsertSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_InsertSort.Location = new System.Drawing.Point(761, 114);
+            this.textBox_InsertSort.Multiline = true;
+            this.textBox_InsertSort.Name = "textBox_InsertSort";
+            this.textBox_InsertSort.Size = new System.Drawing.Size(365, 93);
+            this.textBox_InsertSort.TabIndex = 2;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Start.Location = new System.Drawing.Point(1132, 112);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(149, 40);
+            this.btn_Start.TabIndex = 4;
+            this.btn_Start.Text = "Начать";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Pause
+            // 
+            this.btn_Pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Pause.Location = new System.Drawing.Point(1132, 160);
+            this.btn_Pause.Name = "btn_Pause";
+            this.btn_Pause.Size = new System.Drawing.Size(149, 40);
+            this.btn_Pause.TabIndex = 5;
+            this.btn_Pause.Text = "Пауза";
+            this.btn_Pause.UseVisualStyleBackColor = true;
+            this.btn_Pause.Click += new System.EventHandler(this.btn_Pause_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Stop.Location = new System.Drawing.Point(1132, 206);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(149, 40);
+            this.btn_Stop.TabIndex = 6;
+            this.btn_Stop.Text = "Стоп";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(17, 213);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1112, 33);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // textBox_Random
+            // 
+            this.textBox_Random.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Random.Location = new System.Drawing.Point(430, 30);
+            this.textBox_Random.Multiline = true;
+            this.textBox_Random.Name = "textBox_Random";
+            this.textBox_Random.Size = new System.Drawing.Size(122, 40);
+            this.textBox_Random.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Введите количество случайных чисел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Метод обмена";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(386, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Метод выбора";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(757, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Метод вставок";
+            // 
+            // btn_Generation
+            // 
+            this.btn_Generation.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Generation.Location = new System.Drawing.Point(558, 30);
+            this.btn_Generation.Name = "btn_Generation";
+            this.btn_Generation.Size = new System.Drawing.Size(149, 40);
+            this.btn_Generation.TabIndex = 13;
+            this.btn_Generation.Text = "Создать";
+            this.btn_Generation.UseVisualStyleBackColor = true;
+            this.btn_Generation.Click += new System.EventHandler(this.btn_Generation_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(756, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Полученный массив";
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Result.Location = new System.Drawing.Point(963, 32);
+            this.textBox_Result.Multiline = true;
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.Size = new System.Drawing.Size(318, 40);
+            this.textBox_Result.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1292, 261);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.btn_Generation);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Random);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Pause);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.textBox_InsertSort);
+            this.Controls.Add(this.textBox_ChoiceSort);
+            this.Controls.Add(this.textBox_ChangeSort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox_ChangeSort;
+        private System.Windows.Forms.TextBox textBox_ChoiceSort;
+        private System.Windows.Forms.TextBox textBox_InsertSort;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Pause;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox_Random;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Generation;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_Result;
+    }
+}
+
---
+++ lab_OOP-2-3/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace lab_OOP_2_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        static object locker = new object(); //заглушка
+        static void Print()
+        {
+            lock (locker)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Print1()
+        {
+            ChangeSort(array1);
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                str1 += array1[i];
+                Print();
+            }
+        }
+
+        static void Print2()
+        {
+            ChoiceSort(array1);
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                str2 += array1[i];
+                Print();
+            }
+        }
+
+        static void Print3()
+        {
+            InsertSort(array1);
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                str3 += array1[i];
+                Print();
+            }
+        }
+
+        Thread thread = new Thread(Print);
+        Thread thread1 = new Thread(Print1);
+        Thread thread2 = new Thread(Print2);
+        Thread thread3 = new Thread(Print3);
+
+        static int[] array1;
+        //static int[] array2;
+        //static int[] array3;
+
+        static string str1 = "";
+        static string str2= "";
+        static string str3 = "";
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            textBox_ChangeSort.Text = str1;
+            textBox_ChoiceSort.Text = str2;
+            textBox_InsertSort.Text = str3;
+
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                sum += 1;
+                backgroundWorker1.ReportProgress(i);
+
+                Print();
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    backgroundWorker1.ReportProgress(0);
+                    //thread.Abort();
+                    //thread1.Abort();
+                    //thread2.Abort();
+                    //thread3.Abort();
+                    break;
+                }
+            }
+            e.Result = sum;
+        }
+
+        static void ChangeSort(int[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < A.Length - 1 - i; j++)
+                {
+                    if (A[j] > A[j + 1])
+                    {
+                        int temp = A[j];
+                        A[j] = A[j + 1];
+                        A[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        static void ChoiceSort(int[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                int indMin = i;
+                int min = A[indMin];
+
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    if (A[j] < min)
+                    {
+                        indMin = j;
+                        min = A[j];
+                    }
+                    A[indMin] = A[i];
+                    A[i] = min;
+                }
+            }
+        }
+
+        static void InsertSort(int[] A) 
+        {
+            for (int i = 1; i < A.Length; i++)
+            {
+                int elem = A[i];
+                int pos = 0;
+
+                while (elem > A[pos]) pos++;
+
+                for (int j = i - 1; j >= pos; j--) A[j + 1] = A[j];
+                A[pos] = elem;
+            }
+        }
+
+        [Obsolete]
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            thread.Start();
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            //thread.Start();
+            if (!backgroundWorker1.IsBusy || thread.ThreadState == ThreadState.Unstarted)
+            {
+                backgroundWorker1.RunWorkerAsync();
+                
+            }
+
+            //if (thread.ThreadState == ThreadState.Unstarted)
+            //{
+            //    MessageBox.Show("поток не запущен");
+            //}
+            //else if(thread.ThreadState == ThreadState.Stopped)
+            //{
+            //    thread.Resume();
+            //}
+        }
+
+        //bool flag = true;
+
+        [Obsolete]
+        private void btn_Pause_Click(object sender, EventArgs e)
+        {
+            //thread.Suspend();
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void btn_Generation_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Clear();
+
+            
+            Random random = new Random();
+
+            try
+            {
+                int n = int.Parse(textBox_Random.Text);
+
+                if (n > 7)
+                {
+                    MessageBox.Show("Промежуток цифр от 1 до 7");
+                }
+                else 
+                {
+                    array1 = new int[n];
+
+                    for (int i = 0; i < array1.Length; i++)
+                    {
+                        array1[i] = random.Next(0, 50);
+                    }
+                    for (int i = 0; i < array1.Length; i++)
+                    {
+                        textBox_Result.Text += array1[i].ToString() + "  ";
+                    }
+
+                    //for (int i = 0; i < array1.Length; i++)
+                    //{
+                    //    array2[i] = array1[i];
+                    //    array3[i] = array1[i];
+                    //}
+                }
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show(fe.Message);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled /*|| thread.ThreadState == ThreadState.Stopped*/)
+            {
+                MessageBox.Show("Отмена");
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show("Ошибка");
+            }
+            else
+            {
+                MessageBox.Show("Выполненно");
+            }
+        }
+    }
+}
